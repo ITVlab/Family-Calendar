47eaff12fc67e02729a5dc2ef8c33e50217e5ce5
Nick <handnf@gmail.com>
2016-10-27 00:26:39 -0400

    Added not working Calendar printer

--- app/src/main/java/news/androidtv/familycalendar/MainActivity.java
+++ app/src/main/java/news/androidtv/familycalendar/MainActivity.java
@@ -1,27 +1,51 @@
 package news.androidtv.familycalendar;
 
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.Date;
+import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int CALENDAR_REQUEST = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (PermissionChecker.checkSelfPermission(this, android.Manifest.permission.READ_CALENDAR) != PermissionChecker.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{android.Manifest.permission.READ_CALENDAR}, CALENDAR_REQUEST);
+            } else {
+                readCalendar();
+            }
+        } else {
+            readCalendar();
+        }
     }
 
     private void readCalendar() {
-        Cursor cursor = getContentResolver().query(Uri.parse("content://com.android.calendar/events"), new String[]{ "calendar_id", "title", "description", "dtstart", "dtend", "eventLocation" }, null, null, null);
+        Log.d(TAG, "Read cal");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Cursor cursor = getContentResolver().query(CalendarContract.Events.CONTENT_URI, new String[]{"calendar_id", "title", "description", "dtstart", "dtend", "eventLocation"}, null, null, null);
         //Cursor cursor = cr.query(Uri.parse("content://calendar/calendars"), new String[]{ "_id", "name" }, null, null, null);
         String add = null;
         cursor.moveToFirst();
         String[] CalNames = new String[cursor.getCount()];
+        Log.d(TAG, cursor.getCount() + " entries");
         int[] CalIds = new int[cursor.getCount()];
         for (int i = 0; i < CalNames.length; i++) {
             CalIds[i] = cursor.getInt(0);
@@ -31,10 +55,18 @@ public class MainActivity extends AppCompatActivity {
             else{
                 add += CalNames[i];
             }
-            ((TextView)findViewById(R.id.calendars)).setText(add);
+            Log.d(TAG, add);
 
             cursor.moveToNext();
         }
         cursor.close();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CALENDAR_REQUEST && grantResults[0] == PermissionChecker.PERMISSION_GRANTED) {
+            readCalendar();
+        }
     }
 }
